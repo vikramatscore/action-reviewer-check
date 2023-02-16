e209a98754cb0bc4491a38b656812ef21b298e71
Vikram Kakkar <vikram.kakkar@thescore.com>
2023-02-16 12:01:51 -0500

    - initial

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,49 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const token = core.getInput('github_token') || process.env.GITHUB_TOKEN
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    if (!token) {
+      core.setFailed('❌ Missing Github token')
+      return
+    }
 
-    core.setOutput('time', new Date().toTimeString())
+    const pullRequest = github.context.payload.pull_request
+
+    if (pullRequest) {
+      console.log("Found pull request")
+      const octokit = github.getOctokit(token)
+
+      const {
+        repo: {repo: repoName, owner: repoOwner},
+        runId: runId
+      } = github.context
+      const defaultParameter = {
+        repo: repoName,
+        owner: repoOwner
+      }
+
+      const reviews = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/reviews', {
+        ...defaultParameter,
+        pull_number: pullRequest.number
+      })
+      console.log("reviews: " + reviews.data.length)
+
+      // TODO move to secrets?
+      const reviewTeam = ["akashamin", "bagarwal18", "dariushm2", "jthescore", "kamila-score"]
+      const match = reviews.data.find((review) => {
+        return review.state === "APPROVED" && reviewTeam.includes(`${review.user?.login}`) 
+      })
+      if (!match) {
+        core.setFailed("Mandatory review check failed")
+      }
+    }
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    console.log("Error: " + JSON.stringify(error))
+    if (error instanceof Error) core.setFailed("Caught an error: " + error.message)
   }
 }
 
